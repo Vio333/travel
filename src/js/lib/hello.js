f8d59ab78378cc3877b6fbe7226d19305537d2c8
lemonju <vio1993@outlook.com>
2015-10-26 16:26:43 +0800

    dev

---
+++ src/js/lib/hello.js
@@ -0,0 +1,8 @@
+define(['./say'],function(say){
+	var a = function(){
+		alert("balabala"+say.d);
+	}
+	return {
+		a
+	}
+})
---
+++ src/js/lib/say.js
@@ -0,0 +1,11 @@
+define(function  () {
+	// body...
+	var d = "ddd";
+	var a = function(){
+		alert("lalalal");
+	};
+	return {
+		a,
+		d
+	}
+})
